ff6c76b46f42e62aae5c6cba59d18786551c3cab
diogocbarros <diogocbarros@hotmail.com>
2020-07-15 16:19:36 -0400

    incluindo template

--- main.js
+++ main.js
@@ -1,17 +1,59 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
+
+// Set env
+process.env.NODE_ENV = 'production'
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
+
+let win
+
 
 function createWindow() {
   // Cria uma janela de navegação.
-  let win = new BrowserWindow({
-    width: 800,
+  win = new BrowserWindow({
+    width: 300,
     height: 600,
+    icon: './assets/icons/Icon_256x256.png',
+    backgroundColor: '##d6d5d2',
     webPreferences: {
       nodeIntegration: true
     }
+
   })
 
-  // e carregar o index.html do aplicativo.
-  win.loadFile('index.html')
+  // carregar o index.html do aplicativo.
+  //win.loadFile('./app/index.html')
+  win.loadFile('./_TEMPLATE/index.html')
+  //win.loadURL('http://gacha.club/')
 }
 
-app.whenReady().then(createWindow)
+// Cria o menu
+const menu = [
+  {
+    role: 'fileMenu',
+    // label: 'Diogo',
+    // submenu: [{
+    //   label: 'Quit',
+    //   accelerator: 'CmdOrCtrl+W',
+    //   click: () => app.quit()
+    // }]
+
+  }
+]
+
+function menuPrincipal() {
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
+  win.on('close', () => { app.quit() })
+}
+
+// app.on('ready', () => {
+//   createWindow()
+//   const mainMenu = Menu.buildFromTemplate(menu)
+//   Menu.setApplicationMenu(mainMenu)
+//   win.on('ready', () => win = null)
+// })
+app.whenReady()
+  .then(createWindow)
+  .then(menuPrincipal)
